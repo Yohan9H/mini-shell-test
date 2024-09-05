715c1c76dd914ccf4f4f60ad4cc4e04b12d4b90d
Arthur Pernot <apernot@made-f0Br7s4.clusters.42paris.fr>
2024-09-05 18:16:06 +0200

    repair on init_fd need to understand why

--- src/main.c
+++ src/main.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   main.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: yohurteb <yohurteb@student.42.fr>          +#+  +:+       +#+        */
+/*   By: apernot <apernot@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/01 11:28:39 by yohurteb          #+#    #+#             */
-/*   Updated: 2024/09/05 15:12:32 by yohurteb         ###   ########.fr       */
+/*   Updated: 2024/09/05 18:38:58 by apernot          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -27,8 +27,8 @@ int	main(int ac, char **av, char **env)
 			parser(&data);
 		if (data.code_reset == 0)
 			exec_cmd(&data, env);
-		if (data.code_reset == 0)
-			test_minishell(&data);
+		//if (data.code_reset == 0)
+			//test_minishell(&data);
 		exit_clean(&data, NOTHING, N_EXIT);
 	}
 }
